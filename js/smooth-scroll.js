db849eb1ec9bec331230de02b78159fee7a478a6
Damiant94 <damian.tulacz@gmail.com>
2021-02-13 11:46:02 +0100

    fixed issue with scrolling to section on small width screen

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,17 +1,33 @@
-const sections = [
-    document.querySelector("#about-us-anchor"),
-    document.querySelector("#practis-anchor"),
-    document.querySelector("#lawyers-anchor"),
-    document.querySelector("#publications-anchor"),
-    document.querySelector("#faq-anchor"),
-    document.querySelector("#contact-anchor")
-];
+const createSectionsAnchors = () => {
+    let sections;
+    if (window.matchMedia("(max-width: 1000px)").matches) {
+        sections = [
+            document.querySelector(".about-us"),
+            document.querySelector(".practis"),
+            document.querySelector(".lawyers"),
+            document.querySelector(".publications"),
+            document.querySelector(".faq"),
+            document.querySelector(".contact")
+        ];
+    } else {
+        sections = [
+            document.querySelector("#about-us-anchor"),
+            document.querySelector("#practis-anchor"),
+            document.querySelector("#lawyers-anchor"),
+            document.querySelector("#publications-anchor"),
+            document.querySelector("#faq-anchor"),
+            document.querySelector("#contact-anchor")
+        ];
+    }
+    return sections
+}
 
 const links = document.querySelectorAll('.nav-links a');
 
 links.forEach((link, index) => {
     link.onclick = (event) => {
         event.preventDefault();
+        const sections = createSectionsAnchors();
         sections[index].scrollIntoView({
             behavior: 'smooth'
         })
@@ -20,6 +36,7 @@ links.forEach((link, index) => {
 
 const contactBtn = document.querySelector(".contact-btn");
 contactBtn.onclick = () => {
+    const sections = createSectionsAnchors();
     sections[5].scrollIntoView({
         behavior: 'smooth'
     });
@@ -31,4 +48,9 @@ logo.onclick = () => {
         behavior: 'smooth'
     })
 }
+
+
+
+
+
 
